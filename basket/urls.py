0064798eca85154b35f86fb7ec39367fe888a7d3
Ana Nikolayenia - Karabanava <ananikolayenia@gmail.com>
2024-07-13 19:26:18 +0000

    add: add to basket functionality

--- basket/urls.py
+++ basket/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', basket, name='basket'),
+    path('add/<int:book_id>', add_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 
 def basket(request):
-
+    '''
+    The main basket view.
+    '''
     return render(
         request,
         'basket/basket.html'
     )
+
+
+def add_basket(request, book_id):
+    '''
+    A view for adding items to the basket.
+    '''
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if book_id in list(basket.keys()):
+        basket[book_id] += quantity
+    else:
+        basket[book_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
