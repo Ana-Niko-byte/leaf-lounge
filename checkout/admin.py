e1dd94d6f4ba43929c77ea4943bc89e56e009a62
Ana Nikolayenia - Karabanava <ananikolayenia@gmail.com>
2024-07-21 20:04:46 +0000

    add: signals + register models in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,7 +2,44 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Order)
+class BookLineItemAdminInline(admin.TabularInline):
+    model = BookLineItem
+    readonly_fields = ('order', 'book_order_cost',)
 
 
-admin.site.register(BookLineItem)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (BookLineItemAdminInline,)
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total'
+    )
+    fields = (
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'country',
+        'postcode',
+        'town_city',
+        'street_1',
+        'street_2',
+        'county',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'order_total',
+        'grand_total',
+    )
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import BookLineItem
+
+
+@receiver(post_save, sender=BookLineItem)
+def update_post_save(sender, instance, created, **kwargs):
+    """
+    Updates the order total when booklineitem is updated/created.
+    """
+    instance.order.update_order_total()
+
+
+@receiver(post_delete, sender=BookLineItem)
+def update_post_delete(sender, instance, **kwargs):
+    """
+    Updates the order total when booklineitem is deleted.
+    """
+    instance.order.update_order_total()
