62f0298f2a5e43744fe50283325e3dba6c1d6c25
Ana Nikolayenia - Karabanava <ananikolayenia@gmail.com>
2024-07-22 22:58:51 +0000

    add: publishable key to view + js comments

--- checkout/views.py
+++ checkout/views.py
@@ -19,6 +19,8 @@ def checkout(request):
         request,
         'checkout/checkout.html',
         {
-            'orderForm': order_form
+            'orderForm': order_form,
+            'stripe_public_key': 'pk_test_51PfSh4J7pjNjyBYl3N4sE0VmMv8NXlFPwPz3ZLbiqaqyhUQbwG7F7F95tqVKHBHHfyxmXtS93nEvm7BwHQSfJvLl00qfWIwuGO',
+            'client_secret': 'test client secret key',
         }
     )
