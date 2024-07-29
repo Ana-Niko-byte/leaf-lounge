026538d1f3b8def4122e199671ce21f1b6088457
Ana Nikolayenia - Karabanava <ananikolayenia@gmail.com>
2024-07-29 16:10:52 +0000

    add: stripe webhook function + url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
+from .webhooks import webhook
 
 urlpatterns = [
     path('', checkout, name='checkout'),
-    path('<order_number>/success', success, name='success')
+    path('<order_number>/success', success, name='success'),
+    path('wh/', webhook, name='webhook')
 ]
