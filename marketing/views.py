f3f13b3b89992586f8cb9d72253ffd8c9d2a3492
Ana Nikolayenia - Karabanava <ananikolayenia@gmail.com>
2024-08-07 22:16:39 +0000

    add: setup mailchimp marketing API

--- marketing/views.py
+++ marketing/views.py
@@ -2,23 +2,43 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.conf import settings
 from marketing.forms import EmailForm
+from mailchimp_marketing import Client
+from mailchimp_marketing.api_client import ApiClientError
+import logging
+import hashlib
+
+
+logger = logging.getLogger(__name__)
 
 
 def subscribe_view(request):
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
-            form_email = form.cleaned_data['email']
-            # TODO: use Mailchimp API to subscribe
-            return redirect('subscribe-success')
+            try:
+                form_email = form.cleaned_data['email']
+                member_info = {
+                    'email_address': form_email,
+                    'status': 'subscribed',
+                    'merge_fields': {
+                    'FNAME': 'Elliot',
+                    'LNAME': 'Alderson',
+                    }
+                }
+                response = mailchimp.lists.add_list_member(
+                    settings.MAILCHIMP_MARKETING_AUDIENCE_ID,
+                    member_info,
+                )
+                logger.info(f'API call successful: {response}')
+                return redirect('subscribe-success')
 
-    return render(
-        request,
-        'marketing/subscribe.html',
-        {
-            'form': EmailForm(),
-        }
-    )
+            except ApiClientError as error:
+                logger.error(f'An exception occurred: {error.text}')
+                return redirect('subscribe-fail')
+
+    return render(request, 'marketing/subscribe.html', {
+        'form': EmailForm(),
+    })
 
 
 def subscribe_success_view(request):
@@ -47,17 +67,27 @@ def unsubscribe_view(request):
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
-            form_email = form.cleaned_data['email']
-            # TODO: use Mailchimp API to unsubscribe
-            return redirect('unsubscribe-success')
+            try:
+                form_email = form.cleaned_data['email']
+                form_email_hash = hashlib.md5(form_email.encode('utf-8').lower()).hexdigest()
+                member_update = {
+                    'status': 'unsubscribed',
+                }
+                response = mailchimp.lists.update_list_member(
+                    settings.MAILCHIMP_MARKETING_AUDIENCE_ID,
+                    form_email_hash,
+                    member_update,
+                )
+                logger.info(f'API call successful: {response}')
+                return redirect('unsubscribe-success')
 
-    return render(
-        request,
-        'marketing/unsubscribe.html',
-        {
-            'form': EmailForm(),
-        }
-    )
+            except ApiClientError as error:
+                logger.error(f'An exception occurred: {error.text}')
+                return redirect('unsubscribe-fail')
+
+    return render(request, 'marketing/unsubscribe.html', {
+        'form': EmailForm(),
+    })
 
 
 def unsubscribe_success_view(request):
@@ -80,3 +110,14 @@ def unsubscribe_fail_view(request):
             'message': 'Oops, something went wrong.',
         }
     )
+
+mailchimp = Client()
+mailchimp.set_config({
+  'api_key': settings.MAILCHIMP_API_KEY,
+  'server': settings.MAILCHIMP_REGION,
+})
+
+
+def mailchimp_ping_view(request):
+    response = mailchimp.ping.get()
+    return JsonResponse(response)
