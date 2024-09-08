1adb7a7024b2adef470d0fadd8480700b398e874
Ana Nikolayenia - Karabanava <ananikolayenia@gmail.com>
2024-09-08 09:32:24 +0000

    add: media settings

--- leaves/settings.py
+++ leaves/settings.py
@@ -157,6 +157,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # AWS S3 Bucket Configuration
 if "USE_AWS" in os.environ:
     AWS_STORAGE_BUCKET_NAME = "leaf-lounge"
